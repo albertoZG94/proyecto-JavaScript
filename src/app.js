5bc28bd03a26bcc278b1eece281bf7f080cc1e1e
albertoZG <albertozg94@gmail.com>
2025-05-15 09:25:48 +0000

    proyecto finalizado

--- src/app.js
+++ src/app.js
@@ -2,10 +2,27 @@ import "bootstrap";
 import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ['bombero', 'paco', 'inspector', 'gerente'];
+  let action = ['robaron', 'pidieron prestado', 'estafaron', 'regalaron'];
+  let what = ['mi trabajo', 'mi celular', 'mi carro'];
+  let when = ['Despues de cobrar', 'Cuando sali a comer', 'cuando estaba en el gym', 'en mi almuerzo', 'cuando estaba jugando'];
+ 
+
+
+function generateExcuse() {
+    let part1 = who[Math.floor(Math.random() * who.length)];
+    let part2 = action[Math.floor(Math.random() * action.length)];
+    let part3 = what[Math.floor(Math.random() * what.length)];
+    let part4 = when[Math.floor(Math.random() * when.length)];
+
+    return `${part1} ${part2} ${part3} ${part4}`;
+  }
+  
+  document.getElementById("excusa").innerHTML = generateExcuse();
+
+  const excusa =document.getElementById("excuse");
+  excuseEl.innerHTML = generateExcuse();
 };
